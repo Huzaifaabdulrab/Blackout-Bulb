7ece1d7640cd3157080b38a762549eaf8e381c17
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2024-05-10 13:17:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+"use strict";
+let body = document.querySelector("body");
+let bulbAccess = document.querySelector(".bulb-container");
+let mainBulb = document.querySelector(".bulb");
+let baseBulb = document.querySelector(".base");
+function getHTMLElement() {
+    console.log(body);
+    console.log(bulbAccess);
+    console.log(mainBulb);
+    console.log(baseBulb);
+}
+getHTMLElement();
+let mood = "light";
+mainBulb.addEventListener("click", () => {
+    if (mood === "light") {
+        mood = "dark";
+        body.style.background = "black";
+        baseBulb.innerText = "Light on";
     }
+    else {
+        mood = "light";
+        body.style.background = "white";
+        baseBulb.innerText = "Light off";
+    }
+    console.log(mood);
+});
